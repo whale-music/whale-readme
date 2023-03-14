f3f19e525f595a5b6b66f38998bfb403e048841b
asddfdf <468382010@qq.com>
2023-03-14 22:20:56 +0800

    添加用户收藏操作类

---
+++ core/src/main/java/org/core/mapper/TbUserAlbumMapper.java
@@ -0,0 +1,18 @@
+package org.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.core.pojo.TbUserAlbumPojo;
+
+/**
+ * <p>
+ * 用户收藏专辑表 Mapper 接口
+ * </p>
+ *
+ * @author Sakura
+ * @since 2023-03-14
+ */
+@Mapper
+public interface TbUserAlbumMapper extends BaseMapper<TbUserAlbumPojo> {
+
+}

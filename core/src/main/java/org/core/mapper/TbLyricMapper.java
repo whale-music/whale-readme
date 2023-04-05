f16bb35004bcb194a97dffa72c1cafa62a2249b1
asddfdf <468382010@qq.com>
2023-04-05 13:42:53 +0800

    feat: 添加歌词表

---
+++ core/src/main/java/org/core/mapper/TbLyricMapper.java
@@ -0,0 +1,18 @@
+package org.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.core.pojo.TbLyricPojo;
+
+/**
+ * <p>
+ * 歌词表 Mapper 接口
+ * </p>
+ *
+ * @author Sakura
+ * @since 2023-04-05
+ */
+@Mapper
+public interface TbLyricMapper extends BaseMapper<TbLyricPojo> {
+
+}

a99a23fcbc813a5afca2ff89271a692ddc8509d9
asddfdf <468382010@qq.com>
2023-06-01 19:28:15 +0800

    添加封面表，封面图片使用本地存储

---
+++ core/src/main/java/org/core/iservice/TbPicService.java
@@ -0,0 +1,16 @@
+package org.core.iservice;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import org.core.pojo.TbPicPojo;
+
+/**
+ * <p>
+ * 音乐专辑歌单封面表 服务类
+ * </p>
+ *
+ * @author Sakura
+ * @since 2023-06-01
+ */
+public interface TbPicService extends IService<TbPicPojo> {
+
+}
---
+++ core/src/main/java/org/core/mapper/TbPicMapper.java
@@ -0,0 +1,18 @@
+package org.core.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.core.pojo.TbPicPojo;
+
+/**
+ * <p>
+ * 音乐专辑歌单封面表 Mapper 接口
+ * </p>
+ *
+ * @author Sakura
+ * @since 2023-06-01
+ */
+@Mapper
+public interface TbPicMapper extends BaseMapper<TbPicPojo> {
+
+}

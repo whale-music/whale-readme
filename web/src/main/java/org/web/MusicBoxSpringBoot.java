decdee50123c8155c980a70ddcf3ae9674c6acba
asddfdf <468382010@qq.com>
2023-06-23 22:16:35 +0800

    add jpa config

--- web/src/main/java/org/web/MusicBoxSpringBoot.java
+++ web/src/main/java/org/web/MusicBoxSpringBoot.java
@@ -2,9 +2,13 @@ package org.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EntityScan(basePackages = "org.core.jpa.entity")
+@EnableJpaRepositories(basePackages = "org.core.jpa.repository")
 @SpringBootApplication(scanBasePackages = {"org.core", "org.web", "org.api", "org.oss", "org.plugin"})
 @EnableAsync // 开启任务
 @EnableScheduling // 开启定时任务

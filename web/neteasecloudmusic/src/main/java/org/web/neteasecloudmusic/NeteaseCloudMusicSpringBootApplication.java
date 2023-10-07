63a933623f70f31d00f45e6cda2f259b6971b6ac
asddfdf <468382010@qq.com>
2023-10-07 16:15:46 +0800

    fix: security annotation

--- web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/NeteaseCloudMusicSpringBootApplication.java
+++ web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/NeteaseCloudMusicSpringBootApplication.java
@@ -5,8 +5,13 @@ import org.api.config.EnableApiServer;
 import org.core.factory.YamlPropertySourceFactory;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableApiServer
+// 开启安全校验
+@EnableWebSecurity
+@EnableMethodSecurity
 @PropertySource(value = "classpath:application-neteasecloudmusic.yml", factory = YamlPropertySourceFactory.class)
 @PropertySource("classpath:application-neteasecloudmusic.properties")
 @SpringBootApplication

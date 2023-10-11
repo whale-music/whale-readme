e0113f2eebff63646ef19d9f8462980d8e8aa1a0
asddfdf <468382010@qq.com>
2023-10-11 20:02:57 +0800

    update if framework

--- web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/filter/JwtAuthenticationTokenFilter.java
+++ web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/filter/JwtAuthenticationTokenFilter.java
@@ -43,32 +43,32 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         // 放行运行匿名访问的地址
         // 注意: 如果匿名访问地址中的接口中，获取用户属性则会报错
-        if (permitAllUrlProperties.getUrls().contains(request.getRequestURI()) || PatternMatchUtils.simpleMatch(permitAllUrlProperties.getUrls()
-                                                                                                                                      .toArray(String[]::new),
-                request.getRequestURI())) {
+        if (permitAllUrlProperties.getUrls().contains(request.getRequestURI())
+                || PatternMatchUtils.simpleMatch(permitAllUrlProperties.getUrls().toArray(String[]::new), request.getRequestURI())
+        ) {
             filterChain.doFilter(request, response);
-            return;
-        }
-        Optional<String> first = Arrays.stream(request.getCookies() == null ? new Cookie[]{} : request.getCookies())
-                                       .filter(cookie -> StringUtils.equalsIgnoreCase(CookieConstant.COOKIE_NAME_MUSIC_U, cookie.getName()))
-                                       .map(Cookie::getValue)
-                                       .filter(StringUtils::isNotBlank)
-                                       .findFirst();
-        if (first.isPresent()) {
-            String token = first.get();
-            TokenUtil.checkSign(token);
-            // 校验token, 并获取信息
-            SysUserPojo userPojo = TokenUtil.getInfo(token);
-            if (Objects.nonNull(userPojo)) {
-                UserUtil.setUser(userPojo);
-                UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
-                        null,
-                        AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } else {
+            Optional<String> first = Arrays.stream(request.getCookies() == null ? new Cookie[]{} : request.getCookies())
+                                           .filter(cookie -> StringUtils.equalsIgnoreCase(CookieConstant.COOKIE_NAME_MUSIC_U, cookie.getName()))
+                                           .map(Cookie::getValue)
+                                           .filter(StringUtils::isNotBlank)
+                                           .findFirst();
+            if (first.isPresent()) {
+                String token = first.get();
+                TokenUtil.checkSign(token);
+                // 校验token, 并获取信息
+                SysUserPojo userPojo = TokenUtil.getInfo(token);
+                if (Objects.nonNull(userPojo)) {
+                    UserUtil.setUser(userPojo);
+                    UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
+                            null,
+                            AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
+                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                }
             }
+            filterChain.doFilter(request, response);
+            UserUtil.removeUser();
         }
-        filterChain.doFilter(request, response);
-        UserUtil.removeUser();
     }
 }

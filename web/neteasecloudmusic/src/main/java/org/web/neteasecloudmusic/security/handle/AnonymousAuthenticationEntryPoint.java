2ffbb07f7b89a095590f2cbb687aef25188e897e
asddfdf <468382010@qq.com>
2023-10-11 19:55:00 +0800

    add log

--- web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/handle/AnonymousAuthenticationEntryPoint.java
+++ web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/handle/AnonymousAuthenticationEntryPoint.java
@@ -3,9 +3,11 @@ package org.web.neteasecloudmusic.security.handle;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.core.common.constant.ExceptionPathConstant;
 import org.core.common.exception.BaseException;
 import org.core.common.result.ResultCode;
+import org.core.config.DebugConfig;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -13,11 +15,13 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Optional;
 
 /**
  * 匿名用户访问无权限资源时的异常
  */
 @Component
+@Slf4j
 public class AnonymousAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
     
     @Serial
@@ -25,6 +29,8 @@ public class AnonymousAuthenticationEntryPoint implements AuthenticationEntryPoi
     
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
+        log.error("visit uri {}", request.getRequestURI());
+        Optional.of(DebugConfig.getDebug()).ifPresent(aBoolean -> log.error(authException.getMessage(), authException.fillInStackTrace()));
         request.setAttribute(ExceptionPathConstant.ATTRIBUTE_EXCEPTION_IDENTIFIER, new BaseException(ResultCode.USER_NOT_LOGIN));
         request.getRequestDispatcher(ExceptionPathConstant.ERROR_PATH).forward(request, response);
     }

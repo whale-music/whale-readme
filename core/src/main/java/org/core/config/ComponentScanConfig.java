3014f4337e4c39a6cbd61f53c43ab4a0af406b38
asddfdf <468382010@qq.com>
2023-10-31 17:11:39 +0800

    refactor: remove redundant classes

--- core/src/main/java/org/core/config/ComponentScanConfig.java
+++
@@ -1,9 +0,0 @@
-package org.core.config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan("org.common")
-public class ComponentScanConfig {
-}

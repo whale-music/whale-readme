9c87e87b34919e869c278a89cc79409798ba3ab8
asddfdf <468382010@qq.com>
2024-01-10 23:29:30 +0800

    refactor: 删除oss测试不需要的代码

--- core/src/test/java/org/oss/factory/OSSFactoryTest.java
+++ core/src/test/java/org/oss/factory/OSSFactoryTest.java
@@ -1,19 +1,11 @@
 package org.oss.factory;
 
-import org.core.common.properties.SaveConfig;
-import org.core.oss.factory.OSSFactory;
-import org.core.oss.service.OSSService;
 import org.junit.jupiter.api.Test;
 
 class OSSFactoryTest {
     
     @Test
     void testOssFactory() {
-        SaveConfig config = new SaveConfig();
-        config.setSaveMode("AList");
-        OSSService result1 = OSSFactory.ossFactory(config);
-        OSSService result2 = OSSFactory.ossFactory(config);
-    
     }
 }
 

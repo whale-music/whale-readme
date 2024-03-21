89a9d58a55434eb453399f498b5c30b9c2c1e595
asddfdf <468382010@qq.com>
2024-03-21 23:40:53 +0800

    perf: 抽取缓存key值

---
+++ api/src/main/java/org/api/webdav/constant/WebdavCacheConstant.java
@@ -0,0 +1,11 @@
+package org.api.webdav.constant;
+
+public class WebdavCacheConstant {
+    public static final String WEBDAV_COLLECT_TYPE_LIST = "webdav-collect-type-list";
+    public static final String WEBDAV_PLAY_LIST = "webdav-play-list";
+    public static final String WEBDAV_USER_POJO = "webdav-user-pojo";
+    public static final String WEBDAV_RESOURCE = "webdav-resource";
+    private WebdavCacheConstant() {
+    }
+    
+}

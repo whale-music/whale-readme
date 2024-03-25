8951a1aca2398bd32d3387677db26c74265c9626
asddfdf <468382010@qq.com>
2024-03-25 22:23:25 +0800

    fix: rename sys_log mybatis plus service name

--- core/src/main/java/org/core/mybatis/iservice/SysLogService.java
+++ core/src/main/java/org/core/mybatis/iservice/SysLogService.java
@@ -11,6 +11,6 @@ import org.core.mybatis.pojo.SysLogPojo;
  * @author Sakura
  * @since 2024-03-25
  */
-public interface SysLogoService extends IService<SysLogPojo> {
+public interface SysLogService extends IService<SysLogPojo> {
 
 }
--- core/src/main/java/org/core/mybatis/iservice/impl/SysLogServiceImpl.java
+++ core/src/main/java/org/core/mybatis/iservice/impl/SysLogServiceImpl.java
@@ -1,7 +1,7 @@
 package org.core.mybatis.iservice.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import org.core.mybatis.iservice.SysLogoService;
+import org.core.mybatis.iservice.SysLogService;
 import org.core.mybatis.mapper.SysLogoMapper;
 import org.core.mybatis.pojo.SysLogPojo;
 import org.springframework.stereotype.Service;
@@ -15,6 +15,6 @@ import org.springframework.stereotype.Service;
  * @since 2024-03-25
  */
 @Service
-public class SysLogServiceImpl extends ServiceImpl<SysLogoMapper, SysLogPojo> implements SysLogoService {
+public class SysLogServiceImpl extends ServiceImpl<SysLogoMapper, SysLogPojo> implements SysLogService {
 
 }

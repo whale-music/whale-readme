21a8a720bd6209f8c139542fc1882612f65fe0fa
asddfdf <468382010@qq.com>
2024-03-26 00:29:28 +0800

    feat: 添加异常weblog记录

--- core/src/main/java/org/core/common/weblog/constant/LogNameConstant.java
+++ core/src/main/java/org/core/common/weblog/constant/LogNameConstant.java
@@ -6,6 +6,8 @@ public class LogNameConstant {
     public static final String SUBSONIC = "Subsonic";
     public static final String WEBDAV = "Webdav";
     
+    public static final String REDIRECT_EXCEPTION = "RedirectException";
+    
     private LogNameConstant() {
     }
 }
--- web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/exception/ExceptionController.java
+++ web/neteasecloudmusic/src/main/java/org/web/neteasecloudmusic/security/exception/ExceptionController.java
@@ -4,6 +4,8 @@ import cn.hutool.core.convert.Convert;
 import jakarta.servlet.http.HttpServletRequest;
 import org.core.common.annotation.AnonymousAccess;
 import org.core.common.constant.ExceptionPathConstant;
+import org.core.common.weblog.annotation.WebLog;
+import org.core.common.weblog.constant.LogNameConstant;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class ExceptionController {
-    
+    @WebLog(LogNameConstant.REDIRECT_EXCEPTION)
     @AnonymousAccess
     @RequestMapping(value = ExceptionPathConstant.ERROR_PATH, method = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.PUT, RequestMethod.POST, RequestMethod.TRACE})
     public String errorException(HttpServletRequest request) throws Exception {

67db6d3519a43e27cb165b926ba00ff48adfe4f5
asddfdf <468382010@qq.com>
2024-03-27 11:50:39 +0800

    perf: 优化用户信息校验，如果是匿名接口。校验token时则不抛出错误

--- web/nmusic/src/main/java/org/web/nmusic/security/filter/JwtAuthenticationTokenFilter.java
+++ web/nmusic/src/main/java/org/web/nmusic/security/filter/JwtAuthenticationTokenFilter.java
@@ -14,6 +14,7 @@ import org.core.utils.RoleUtil;
 import org.core.utils.UserUtil;
 import org.core.utils.token.TokenUtil;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -45,56 +46,96 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     }
     
     
+    /**
+     * 从cookie header parameter body 中获取token
+     *
+     * @param request 请求
+     * @return token
+     */
+    @Nullable
+    private static String getToken(@NotNull HttpServletRequest request) {
+        // 获取cookie
+        Optional<String> first = Arrays.stream(request.getCookies() == null ? new Cookie[]{} : request.getCookies())
+                                       .filter(cookie -> StringUtils.equalsIgnoreCase(CookieConstant.COOKIE_NAME_MUSIC_U, cookie.getName()))
+                                       .map(Cookie::getValue)
+                                       .filter(StringUtils::isNotBlank)
+                                       .findFirst();
+        // cookie中没有则从其他参数中获取
+        String token = null;
+        if (first.isPresent()) {
+            token = first.get();
+        } else {
+            try {
+                // get parameters
+                String cookie = request.getParameterMap().get("cookie")[0];
+                if (StringUtils.isBlank(cookie)) {
+                    // post body
+                    cookie = IOUtils.toString(request.getReader());
+                }
+                token = cookie.split("=")[1];
+            } catch (Exception e) {
+                log.error("Failed to parse cookie, origin msgs: {}", e.getMessage());
+            }
+        }
+        return token;
+    }
+    
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         // 放行运行匿名访问的地址
-        // 注意: 如果匿名访问地址中的接口中，获取用户属性则会报错
-        if (permitAllUrlProperties.getUrls().contains(request.getRequestURI())
-                || PatternMatchUtils.simpleMatch(permitAllUrlProperties.getUrls().toArray(String[]::new), request.getRequestURI())
-        ) {
+        String token = getToken(request);
+        if (mathUri(request)) {
+            // 如果是匿名接口，校验则不抛出异常
+            if (validate(token)) {
+                setSecurityUser(request, token);
+            }
             filterChain.doFilter(request, response);
         } else {
-            // 获取cookie
-            Optional<String> first = Arrays.stream(request.getCookies() == null ? new Cookie[]{} : request.getCookies())
-                                           .filter(cookie -> StringUtils.equalsIgnoreCase(CookieConstant.COOKIE_NAME_MUSIC_U, cookie.getName()))
-                                           .map(Cookie::getValue)
-                                           .filter(StringUtils::isNotBlank)
-                                           .findFirst();
-            // cookie中没有则从其他参数中获取
-            String token = null;
-            if (first.isPresent()) {
-                token = first.get();
-            } else {
-                try {
-                    // get parameters
-                    String cookie = request.getParameterMap().get("cookie")[0];
-                    if (StringUtils.isBlank(cookie)) {
-                        // post body
-                        cookie = IOUtils.toString(request.getReader());
-                    }
-                    token = cookie.split("=")[1];
-                } catch (Exception e) {
-                    log.error("Failed to parse cookie, origin msgs: {}", e.getMessage());
-                }
-            }
             try {
                 if (StringUtils.isNotBlank(token)) {
                     tokenUtil.checkSign(token);
-                    // 校验token, 并获取信息
-                    SysUserPojo userPojo = tokenUtil.getUserInfo(token);
-                    if (Objects.nonNull(userPojo)) {
-                        UserUtil.setUser(userPojo);
-                        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
-                                null,
-                                AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
-                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-                    }
+                    setSecurityUser(request, token);
                 }
                 filterChain.doFilter(request, response);
             } finally {
                 UserUtil.removeUser();
             }
+        }
+    }
+    
+    private void setSecurityUser(@NotNull HttpServletRequest request, String token) {
+        // 校验token, 并获取信息
+        SysUserPojo userPojo = tokenUtil.getUserInfo(token);
+        if (Objects.nonNull(userPojo)) {
+            UserUtil.setUser(userPojo);
+            UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
+                    null,
+                    AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
+            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+    }
+    
+    private boolean mathUri(@NotNull HttpServletRequest request) {
+        return permitAllUrlProperties.getUrls().contains(request.getRequestURI())
+                || PatternMatchUtils.simpleMatch(permitAllUrlProperties.getUrls().toArray(String[]::new), request.getRequestURI());
+    }
+    
+    /**
+     * 成功 true 失败false
+     *
+     * @param token token
+     * @return flag
+     */
+    private boolean validate(String token) {
+        if (StringUtils.isBlank(token)) {
+            return false;
+        }
+        try {
+            tokenUtil.checkSign(token);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 }

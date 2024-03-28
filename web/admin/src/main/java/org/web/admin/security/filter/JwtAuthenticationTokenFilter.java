1f6125853264815e5f233eedaecbf9af89c4fbab
asddfdf <468382010@qq.com>
2024-03-28 23:50:10 +0800

    perf: 优化admin前端用户校验

--- web/admin/src/main/java/org/web/admin/security/filter/JwtAuthenticationTokenFilter.java
+++ web/admin/src/main/java/org/web/admin/security/filter/JwtAuthenticationTokenFilter.java
@@ -10,7 +10,6 @@ import org.core.mybatis.pojo.SysUserPojo;
 import org.core.utils.RoleUtil;
 import org.core.utils.UserUtil;
 import org.core.utils.token.TokenUtil;
-import org.core.utils.token.UserCacheServiceUtil;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.AuthorityUtils;
@@ -18,9 +17,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.web.admin.security.config.AdminPermitAllUrlProperties;
 
 import java.io.IOException;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * token过滤器 验证token有效性，然后注入到线程变量中
@@ -32,15 +33,23 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     
     private final TokenUtil tokenUtil;
     
-    private final UserCacheServiceUtil userCacheServiceUtil;
+    private final AdminPermitAllUrlProperties adminPermitAllUrlProperties;
     
-    public JwtAuthenticationTokenFilter(TokenUtil tokenUtil, UserCacheServiceUtil userCacheServiceUtil) {
+    public JwtAuthenticationTokenFilter(TokenUtil tokenUtil, AdminPermitAllUrlProperties adminPermitAllUrlProperties) {
         this.tokenUtil = tokenUtil;
-        this.userCacheServiceUtil = userCacheServiceUtil;
+        this.adminPermitAllUrlProperties = adminPermitAllUrlProperties;
+    }
+    
+    private static void fillAuthUserToken(@NotNull HttpServletRequest request, SysUserPojo userPojo) {
+        UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
+                null,
+                AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
     
     /**
-     * 获取到真实的 token 串
+     * 获取到真实的 token 字符串
      *
      * @param authorizationToken token
      * @return 真实token
@@ -52,7 +61,9 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(@NotNull HttpServletRequest request, @NotNull HttpServletResponse response, @NotNull FilterChain filterChain) throws ServletException, IOException {
         String token = getRealAuthorizationToken(request.getHeader(Header.AUTHORIZATION.name()));
-        if (StringUtils.isBlank(token)) {
+        // 如果是匿名接口或者没有token都放行
+        Set<String> urls = adminPermitAllUrlProperties.getUrls();
+        if (urls.contains(request.getRequestURI()) || StringUtils.isBlank(token)) {
             filterChain.doFilter(request, response);
             return;
         }
@@ -60,25 +71,17 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
             // 校验token
             tokenUtil.isJwtExpired(token);
             tokenUtil.checkSign(token);
+            // 获取用户信息
+            SysUserPojo userPojo = tokenUtil.getUserInfo(token);
+            // 没有用户则之间跳过
+            if (Objects.isNull(userPojo)) {
+                filterChain.doFilter(request, response);
+                return;
+            }
+            UserUtil.setUser(userPojo);
+            fillAuthUserToken(request, userPojo);
+            filterChain.doFilter(request, response);
         } catch (Exception e) {
-            userCacheServiceUtil.clearUserCache(token);
-            filterChain.doFilter(request, response);
-            return;
-        }
-        
-        // 校验token, 并获取信息
-        SysUserPojo userPojo = tokenUtil.getUserInfo(token);
-        if (Objects.isNull(userPojo)) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-        UserUtil.setUser(userPojo);
-        try {
-            UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken.authenticated(userPojo,
-                    null,
-                    AuthorityUtils.createAuthorityList(RoleUtil.getRoleNames(userPojo.getRoleName())));
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
             filterChain.doFilter(request, response);
         } finally {
             UserUtil.removeUser();

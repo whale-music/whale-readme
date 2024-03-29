4e7c9190f0b81e4c572d3b00b15c8538a0baf6fe
asddfdf <468382010@qq.com>
2024-03-29 10:30:03 +0800

    feat: add CaffeineCacheManager to @Bean

--- core/src/main/java/org/core/config/CacheConfig.java
+++ core/src/main/java/org/core/config/CacheConfig.java
@@ -6,6 +6,9 @@ import org.core.model.PicMiddleTypeModel;
 import org.core.model.UserLoginCacheModel;
 import org.core.model.WebDavResource;
 import org.core.mybatis.pojo.TbPicPojo;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +16,24 @@ import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class CacheConfig {
+    
+    /**
+     * 配置缓存管理器, ${@link Cacheable}注解缓存管理器
+     *
+     * @return 缓存管理器
+     */
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+                                         // 设置最后一次写入或访问后经过固定时间过期, 一天
+                                         .expireAfterAccess(3, TimeUnit.HOURS)
+                                         // 初始的缓存空间大小
+                                         .initialCapacity(100)
+                                         // 缓存的最大条数
+                                         .maximumSize(1000));
+        return cacheManager;
+    }
     
     /**
      * 图片缓存

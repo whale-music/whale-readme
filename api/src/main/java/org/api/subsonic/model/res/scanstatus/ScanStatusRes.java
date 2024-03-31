a2bdc3f2e9a39789d3608c4d0f393ffa5708d69f
asddfdf <468382010@qq.com>
2024-03-31 18:58:31 +0800

    feat: 优化/getScanStatus返回数据结构

--- api/src/main/java/org/api/subsonic/model/res/scanstatus/ScanStatusRes.java
+++ api/src/main/java/org/api/subsonic/model/res/scanstatus/ScanStatusRes.java
@@ -1,5 +1,7 @@
 package org.api.subsonic.model.res.scanstatus;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,15 +14,28 @@ import org.api.subsonic.common.SubsonicResult;
 @EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 public class ScanStatusRes extends SubsonicResult {
+    
+    @JsonProperty("scanStatus")
+    @JacksonXmlElementWrapper(useWrapping = false)
     private ScanStatus scanStatus;
     
     @Data
     @AllArgsConstructor
     public static class ScanStatus {
+        @JsonProperty("lastScan")
+        @JacksonXmlProperty(isAttribute = true)
+        private String lastScan;
+        
+        @JsonProperty("scanning")
         @JacksonXmlProperty(isAttribute = true)
         private Boolean scanning;
         
+        @JsonProperty("count")
         @JacksonXmlProperty(isAttribute = true)
         private Long count;
+        
+        @JsonProperty("folderCount")
+        @JacksonXmlProperty(isAttribute = true)
+        private Integer folderCount;
     }
 }

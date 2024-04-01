12065a8f34bdc101bfba54d3bee259f800b0e17c
asddfdf <468382010@qq.com>
2024-04-01 11:20:00 +0800

    perf: subsonic user verification

--- api/src/main/java/org/api/subsonic/aspect/ManualSerializeAspect.java
+++ api/src/main/java/org/api/subsonic/aspect/ManualSerializeAspect.java
@@ -77,25 +77,39 @@ public class ManualSerializeAspect {
         }
     }
     
+    /**
+     * 兼容1.12.0之前的版本
+     *
+     * @param user     用户名
+     * @param password 用户密码
+     * @param token    token 高版本会用到
+     * @param salt     盐
+     * @param version  客户端版本
+     */
     private void authenticate(String user, String password, String token, String salt, String version) {
-        int compare = VersionComparator.INSTANCE.compare(version, "1.12.0");
         // 目标API版本为1.12.0或更低版本，则通过以明文或十六进制编码的形式发送密码来执行身份验证。示例：
         // http://your-server/rest/ping.view?u=joe&p=sesame&v=1.12.0&c=myapp
         // http://your-server/rest/ping.view?u=joe&p=enc:736573616d65&v=1.12.0&c=myapp
-        if (compare > 0 || StringUtils.isBlank(password) || StringUtils.equals("null", password)) {
+        
+        // 大于1.12版本，而且必须不是以enc:开头, password字段为空。使用身份验证令牌
+        int compare = VersionComparator.INSTANCE.compare(version, "1.12.0");
+        boolean protocolCheckflag = compare > 0 && !StringUtils.startsWithIgnoreCase(password, "enc:");
+        if (protocolCheckflag || StringUtils.isBlank(password)) {
             // 从API版本1.13.0开始，推荐的身份验证方案是发送身份验证令牌，计算为密码的单向加盐哈希。
             // This involves two steps: 这涉及两个步骤：
-            //
+            
             // For each REST call, generate a random string called the salt. Send this as parameter s.
-            //         对于每个REST调用，生成一个称为salt的随机字符串。将其作为参数 s 发送。
+            // 对于每个REST调用，生成一个称为salt的随机字符串。将其作为参数 s 发送。
+            
             // Use a salt length of at least six characters.
-            //         使用至少六个字符的salt长度。
+            // 使用至少六个字符的salt长度。
+            
             // Calculate the authentication token as follows: token = md5(password + salt). The md5() function takes a string and returns the 32-byte ASCII hexadecimal representation of the MD5 hash, using lower case characters for the hex values. The '+' operator represents concatenation of the two strings. Treat the strings as UTF-8 encoded when calculating the hash. Send the result as parameter t.
-            // 计算身份验证令牌，如下所示：token = md5（password + salt）。md5（）函数接受一个字符串，并返回MD5哈希的32字节ASCII十六进制表示，十六进制值使用小写字符。“+”运算符表示两个字符串的连接。在计算哈希时，将字符串视为UTF-8编码。将结果作为参数 t 发送。
-            //
+            // 计算身份验证令牌，如下所示：token = md5（password + salt. md5（）函数接受一个字符串，并返回MD5哈希的32字节ASCII十六进制表示，十六进制值使用小写字符。“+”运算符表示两个字符串的连接。在计算哈希时，将字符串视为UTF-8编码。将结果作为参数 t 发送。
+            
             // For example: if the password is sesame and the random salt is c19b2d, then token = md5("sesamec19b2d") = 26719a1196d2a940705a59634eb18eab. The corresponding request URL then becomes:
             // 例如：如果口令是sesame，随机盐是c19 b2 d，则token = md5（“sesamec 19 b2 d”）= 26719 a1196 d2 a940705 a59634 eb 18 eab。相应的请求URL变为：
-            //
+            
             // http://your-server/rest/ping.view?u=joe&t=26719a1196d2a940705a59634eb18eab&s=c19b2d&v=1.12.0&c=myapp
             SysUserPojo pojo = subsonicUserCache.checkUser(user);
             String originToken = SecureUtil.md5(pojo.getPassword() + salt);
